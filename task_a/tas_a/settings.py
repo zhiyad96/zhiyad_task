413d97304d2cec48dc9ce24dec049c30e2b51553
zhiyad96 <mhdzhiyad9656@gmail.com>
2026-02-21 13:41:44 +0530

    Initial commit - User Management Mini Application

--- task_a/tas_a/settings.py
+++ task_a/tas_a/settings.py
@@ -12,12 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
